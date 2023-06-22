2f33421e45fddddd1b8044f234bb18a24163d10a
S E Prince Joseph <sindala.prince@gmail.com>
2023-06-22 16:28:59 +0530

    + feedback form generation

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import QuestionPermit, Question
 
-# Create your views here.
+
+def feedback_view(request):
+    permitted_questions_list = QuestionPermit.objects.filter(
+        designation__id=request.user.designation.id
+    ).values_list('id')
+    questions = Question.objects.filter(id__in = permitted_questions_list)
+    context = {"questions": questions}
+    return render(request, "feedback_form.html", context)
